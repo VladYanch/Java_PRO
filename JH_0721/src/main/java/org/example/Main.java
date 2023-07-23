0d32d224d7de49a399b51f76199b6a439ec8e6ba
Vladyslav Yanchenko <vyanch72@gmail.com>
2023-07-23 07:35:38 +0200

    HW Dev+Car 07.21

--- JH_0721/src/main/java/org/example/Main.java
+++ JH_0721/src/main/java/org/example/Main.java
@@ -6,8 +6,7 @@ public class Main {
     public static void main(String[] args) {
         List<Developer> developers = Generator.generateDeveloper(20);
         System.out.println("-".repeat(100));
-
-
+        
         System.out.println("SportCar = speed>280");
         System.out.println(Reports.findAllManSportCar(developers));
         System.out.println("-".repeat(100));
